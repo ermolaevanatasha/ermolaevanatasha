9677dc92637f3c1b3d02a05cf645f1cf3ccdeb6e
Natasha <litegoldfish@yandex.ru>
2016-12-19 17:08:46 +0300

    EXAM

--- test_work.py
+++ test_work.py
@@ -27,17 +27,20 @@ def find_matches():
 def rus_nouns():
     inp = '/Users/nata/Desktop/'
     os.system(r'/Users/nata/Desktop/mystem ' + inp + 'wordlist.txt' + ' ' + 'rus.txt' + ' -ind')
-    f = open('rus.txt', 'r', encoding='utf-8')
-    string = f.read()
-    pattern = 'S,.*?,.*?=им,ед'
-    for word in string:
-        m = re.search(pattern, word)
-        if m:
-            reg_word = re.compile('^[^\{]+', flags=re.U | re.DOTALL) 
-            words = reg_word.findall(word)
-            for word in words:
-                with open('rus_nouns.txt', 'w', encoding='utf-8') as file:
-                    file.write(word + '\n')
+    with open('rus.txt', 'r', encoding='utf-8') as f:
+        rus_words = set()
+
+        for line in f:
+            m = re.search(r'=S,[^=]+=им,ед', line)
+            if m:
+                m_word = re.search(r'^[^\{]+', line)
+                if m_word:
+                    rus_words.add(m_word.group(0))
+
+    with open('rus_nouns.txt', 'w', encoding='utf-8') as f:
+        f.writelines(['%s\n' % word for word in rus_words])
+                    
+    
 
 def main():
     find_matches()
