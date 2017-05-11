13955f0432986d1b309ffcd0b279eba745c46967
Natasha Ermolaeva <litegoldfish@yandex.ru>
2017-05-11 23:44:39 +0300

    my_bot

---
+++ TelegramBot/icancountwords_bot.py
@@ -0,0 +1,53 @@
+import flask
+import telebot
+import conf
+
+WEBHOOK_URL_BASE = "https://{}".format(conf.WEBHOOK_HOST)
+WEBHOOK_URL_PATH = "/{}".format(conf.TOKEN)
+
+bot = telebot.TeleBot(conf.TOKEN, threaded=False)
+bot.remove_webhook()
+bot.set_webhook(url=(WEBHOOK_URL_BASE+WEBHOOK_URL_PATH))
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET', 'HEAD'])
+def index():
+    return ''
+
+@app.route(WEBHOOK_URL_PATH, methods=['POST'])
+def webhook():
+    if flask.request.headers.get('content-type') == 'application/json':
+        json_string = flask.request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        flask.abort(403)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, 'Здравствуйте! Этот бот считает количество слов в вашем сообщении.')
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.send_message(message.chat.id, 'Просто отправьте боту сообщение, длину которого вы хотите посчитать.')
+
+@bot.message_handler(func=lambda m: True)
+def send_len(message):
+    words = message.text.replace(' - ', ' ').replace(' \u2013 ', ' ').replace(' \u2014 ', ' ').split()
+    wordCount = len(words)
+
+    mod10 = wordCount % 10
+    mod100 = wordCount % 100
+
+    form = 'слов'
+
+    if 10 < mod100 < 20:
+        pass
+    elif mod10 == 1:
+        form = 'слово'
+    elif 1 < mod10 < 5:
+        form = 'слова'
+
+    bot.send_message(message.chat.id, 'В вашем сообщении {} {}.'.format(wordCount, form))
